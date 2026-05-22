a193a3f0e03b8c8ca174d640e0d4ce1494ce06bb
Abdul Samad K A <85991146+nuruszama@users.noreply.github.com>
2026-05-22 11:34:15 +0400

    Update build_header.sh

--- config/build_header.sh
+++ config/build_header.sh
@@ -4,6 +4,15 @@ sudo rm -f /etc/localtime
 sudo ln -s /usr/share/zoneinfo/${TZ} /etc/localtime
 echo "🕒 Current system time: $(date)"
 
+# ================= CONFIGS =================
+if [ ! -f "uploads_logic.sh" ]; then
+    echo "Fetching uploads_logic.sh"
+    curl -sf https://raw.githubusercontent.com/nuruszama/crave_build_scripts/lineage-23.2/config/uploads_logic.sh -o uploads_logic.sh
+fi
+echo "Loading uploads_logic.sh"
+source uploads_logic.sh
+rm -rf build_config.sh
+
 # ================= BUILD START =================
 tg_send "┌───────────────────┐
   📢      *Buildbot* initialized      📢
