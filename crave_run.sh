7e894a4f804f7ec39206cbd1c6d033eafa95e39e
Safwan Eh S <ehfazinfo@gmail.com>
2026-05-24 11:48:30 +0600

    fix: mkdir -p before kernel/xiaomi/creek symlink

--- crave_run.sh
+++ crave_run.sh
@@ -60,6 +60,7 @@ rm -rf hardware/qcom-caf/common
 git clone https://github.com/sapphire-sm6225/android_hardware_qcom-caf_common.git -b lineage-23.2 hardware/qcom-caf/common
 
 # Symlink kernel headers for generated_kernel_includes
+mkdir -p kernel/xiaomi
 ln -sf device/xiaomi/creek-kernel kernel/xiaomi/creek
 
 SYNC_END=$(date +%s)
