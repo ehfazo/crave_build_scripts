271beb21ab6c3edea932031e38afd78c14949892
Safwan Eh S <ehfazinfo@gmail.com>
2026-05-24 17:12:29 +0600

    fix: handle existing git remote origin in build_vps.sh

--- build_vps.sh
+++ build_vps.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 git init
-git remote add origin https://github.com/accupara/los22.git
+if ! git remote get-url origin &>/dev/null; then
+    git remote add origin https://github.com/accupara/los22.git
+else
+    git remote set-url origin https://github.com/accupara/los22.git
+fi
 curl -sf https://raw.githubusercontent.com/ehfazo/crave_build_scripts/lineage-23.2/crave_build.sh | bash
